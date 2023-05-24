6420c4503a12ebdc9e6c25d6e690a5eaff912b77
gwiyeomgo <gwiyeomgo@gmail.com>
2023-05-24 18:01:02 +0900

    first commit

---
+++ go.mod
@@ -0,0 +1,23 @@
+module goSiren
+
+go 1.19
+
+require (
+	github.com/labstack/echo v3.3.10+incompatible
+	github.com/mssola/user_agent v0.6.0
+	github.com/pkg/errors v0.9.1
+	github.com/sirupsen/logrus v1.9.2
+	golang.org/x/exp v0.0.0-20230522175609-2e198f4a06a1
+)
+
+require (
+	github.com/labstack/gommon v0.4.0 // indirect
+	github.com/mattn/go-colorable v0.1.11 // indirect
+	github.com/mattn/go-isatty v0.0.14 // indirect
+	github.com/valyala/bytebufferpool v1.0.0 // indirect
+	github.com/valyala/fasttemplate v1.2.1 // indirect
+	golang.org/x/crypto v0.9.0 // indirect
+	golang.org/x/net v0.10.0 // indirect
+	golang.org/x/sys v0.8.0 // indirect
+	golang.org/x/text v0.9.0 // indirect
+)
---
+++ goSiren.go
@@ -0,0 +1,112 @@
+package goSiren
+
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/labstack/echo"
+	"github.com/mssola/user_agent"
+	"github.com/sirupsen/logrus"
+	"golang.org/x/exp/slices"
+	"net/http"
+)
+
+const EnvironmentStaging = "staging"
+const EnvironmentProduction = "production"
+const EnvironmentLocal = "local"
+const EnvironmentDev = "dev"
+
+type GoSiren struct {
+	TargetCodes        []int
+	TargetEnvironments []string
+}
+type Content interface {
+	make() (string, string, error)
+}
+
+func (c *GoSiren) IsSending(code int, environment string) bool {
+	if slices.Contains(c.TargetEnvironments, environment) && slices.Contains(c.TargetCodes, code) {
+		return true
+	}
+	return false
+}
+
+func (c *GoSiren) Setting(content Content) (string, string, error) {
+	return content.make()
+}
+
+type Alarm struct {
+	Request    *http.Request
+	Code       int
+	StackTrace string
+	UserID     int
+	UserName   string
+}
+
+func (g *Alarm) make() (string, string, error) {
+	token := g.Request.Header.Get(echo.HeaderAuthorization)
+
+	title := g.StackTrace
+	if len(g.StackTrace) > 40 {
+		title = g.StackTrace[:40]
+	}
+
+	content := fmt.Sprintf("# 로그인 사용자\n"+
+		"* memberId:<span style=\"color:#9933ff\"> %v</span>\n"+
+		"* Name:<span style=\"color:#9933ff\"> %v</span>\n\n"+
+		"# HTTP Request\n"+
+		"* HOST :<span style=\"color:#9933ff\"> %v</span>\n"+
+		"* URL :<span style=\"color:#9933ff\"> %v</span>\n"+
+		"* Method :<span style=\"color:#9933ff\"> %v</span>\n* Header\n    "+
+		"* Content-Type:<span style=\"color:#9933ff\"> %v</span>\n    "+
+		"* Authorization\n```\n%v\n```\n"+
+		"* BrowserUserAgent\n```\n%v\n```\n"+
+		"* Body\n```\n%v\n```\n\n"+
+		"# HTTP Response\n"+
+		"* status code : <span style=\"color:#9933ff\">%v</span>\n"+
+		"# Error 원인\n```\n%v\n```",
+		g.UserID,
+		g.UserName,
+		g.Request.Host,
+		g.Request.RequestURI,
+		g.Request.Method,
+		g.Request.Header.Get(echo.HeaderContentType),
+		token,
+		getHumanizeBrowserUserAgent(g.Request.UserAgent()),
+		g.Request.Body,
+		g.Code,
+		fmt.Sprintf("[ERROR] %v \n", g.StackTrace))
+
+	return title, content, nil
+}
+
+func getHumanizeBrowserUserAgent(browserUserAgent string) string {
+	if len(browserUserAgent) == 0 {
+		return ""
+	}
+	ua := user_agent.New(browserUserAgent)
+
+	humanizeUserAgent := map[string]interface{}{}
+	humanizeUserAgent["mobile"] = ua.Mobile()
+	humanizeUserAgent["platform"] = ua.Platform()
+	humanizeUserAgent["os"] = ua.OS()
+
+	name, version := ua.Engine()
+	engine := map[string]interface{}{}
+	engine["name"] = name
+	engine["version"] = version
+	humanizeUserAgent["engine"] = engine
+
+	name, version = ua.Browser()
+	browser := map[string]interface{}{}
+	browser["name"] = name
+	browser["version"] = version
+	humanizeUserAgent["browser"] = browser
+
+	jsonString, err := json.Marshal(humanizeUserAgent)
+	if err != nil {
+		logrus.Info(err)
+		return ""
+	}
+
+	return string(jsonString)
+}
